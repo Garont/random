5a7c5a0f61aa14988e1338467573cebcb8212442
Roman <rashkur@gmail.com>
2016-07-30 16:04:05 +0300

    upd

--- atop2php/atop.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TXT='/var/www/site/atop.txt'
-while true; do
-        atop | head -30 > $TXT
-        echo "<br /> <br />" >> $TXT
-        cat /proc/mdstat >> $TXT
-        sleep 10
-done
